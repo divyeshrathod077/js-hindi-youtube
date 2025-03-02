a05304e1e63f059a7e8dd96a95bda3efc5513ad4
divyeshrathod077 <rathoddivyesh5911@gmail.com>
2025-03-02 06:17:01 +0000

    container configuration done

--- 01_basic/03_conversiononoperation.js
+++ 01_basic/03_conversiononoperation.js
@@ -1,13 +1,7 @@
-let score=33
-console.log(typeof score);
-let valueInnumber=Number(score);
-console.log(typeof valueInnumber);
-console.log(valueInnumber);
-let isLoggenIn =1
-let booleanIsLoggedIn=Boolean(isLoggenIn)
-//console.log(booleanIsLoggedIn);
 
-let someNumber=34
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+
+//*********************operations ******//
+
+let value =3
+let negValue=-value
+//console.log(negValue);
