6915c04b3e0eb3c79b2289bd8ab7b8d088485f1e
divyeshrathod077 <rathoddivyesh5911@gmail.com>
2025-03-02 05:29:47 +0000

    container configuration done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14425
+let acoountEmail ="divyesh@google.com"
+var accountpassword ="12345"
+accountcity="jaipur"
+let accountstate;
+
+//accountId=2//not allowed
+accountEmail="hc@hc.com"
+accountpassword="21212121"
+accountcity="bengalurur"
+
+console.log(accountId);
+console.log([accountId ,accountEmail,accountpassword,accountcity,accountstate]);
+
