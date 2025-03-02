ec303dec1581ca3b2bcd5826313764c26ca314d4
divyeshrathod077 <rathoddivyesh5911@gmail.com>
2025-03-02 08:09:10 +0000

    container configuration done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+//#primitive
+//7 types :string ,number,boolean,null,undefined,symbol,bigint
+
+//reference type (non primitive)
+//arrays,objects,functions
+const score = 100
+const scoreValue=100.3
+const isLoggedIn=false
+let userEmail;
+const id=Symbol('123')
+const anotherId=symbol('123')
+console.log(id==anotherId);
+
