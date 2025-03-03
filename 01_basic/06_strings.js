8b7edc8077cd37f0ac392e6443d8450b4df0d7ba
divyeshrathod077 <rathoddivyesh5911@gmail.com>
2025-03-03 06:37:06 +0000

    container configuration done

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,10 @@
+const name="divyesh"
+const repoCount=50
+//console.log(name+repoCount +"value");
+console.log('hello my name is ${name} and my repo count js ${repoCount}');
+
+const gameName =new String('divyesh')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameNN);
